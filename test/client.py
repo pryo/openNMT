0c0a182997fbfb238993ef682ca3932ef309c544
zuo wang <w.zuo@hotmail.com>
2019-08-13 14:48:38 -0400

    finished and tested the flask framework under gunicorn

--- test/client.py
+++ test/client.py
@@ -1,7 +1,7 @@
 import requests
 import json
 if __name__ == '__main__':
-    local_url = ' http://0.0.0.0:5000/translator/translate'
+    local_url = ' http://0.0.0.0:5000/translate'
     # files = {'picture':open(input('image path: '),'rb'),'beam_size':input('beam size: ')}
     # files = {'picture': open(input('image path: '), 'rb')}
     # r = requests.post(local_url,params = {'beam_size':input('beam size: ')},
