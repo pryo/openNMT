1fdbb62950d7a5042b185ad835dc4b5f6a1aba52
zuo wang <w.zuo@hotmail.com>
2019-08-14 10:00:51 -0400

    created automation command

--- test/client.py
+++ test/client.py
@@ -2,9 +2,11 @@ import requests
 import json
 if __name__ == '__main__':
     local_url = ' http://0.0.0.0:5000/translate'
+    google_url = 'http://35.244.201.148/translate'
+
     # files = {'picture':open(input('image path: '),'rb'),'beam_size':input('beam size: ')}
     # files = {'picture': open(input('image path: '), 'rb')}
     # r = requests.post(local_url,params = {'beam_size':input('beam size: ')},
     #                   files=files,timeout=400)
-    r= requests.post(local_url, json=[{"id": 100, "src": "what is this."}])
+    r= requests.post(google_url, json=[{"id": 100, "src": "what is this."}])
     print(r.text)
